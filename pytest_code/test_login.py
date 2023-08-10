066e683c7b6ef27ff4d7f6f5c71eed35399ade06
Rupen Patel <60769006+rupen08@users.noreply.github.com>
2023-08-09 20:40:15 -0400

    pytest file 1

--- pytest_code/test_login.py
+++ pytest_code/test_login.py
@@ -1,3 +1,13 @@
+## for pytest, there is a name convention that needs to be used
+
+## for .py file name, it could be either test_*.py OR *_test.py
+
+## for any method or function, start with the keyword "test"
+
+## examples are given below
+
+## vscode or any IDE needs a framework to work with pytest
+
 import pytest
 
 def testlogin():
